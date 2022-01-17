fcd195dfef62b767595e3ec8f96a8f2cd3ab2eb1
Pedro Toribio <pedrom_t@hotmail.com>
2022-01-17 21:26:21 +0100

    [Setup] Adding react-icons

---
+++ src/components/pages/Navbar.js
@@ -0,0 +1,17 @@
+import React from "react";
+
+function Navbar() {
+  return (
+    <>
+      <div className="navbar">
+        <div className="navbar-container container">
+          <Link to='/' className="navbar-logo">
+            LAVISH
+          </Link>
+        </div>
+      </div>
+    </>
+  );
+}
+
+export default Navbar;
