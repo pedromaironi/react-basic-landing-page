7b621f7e6cdfb63000368504807826dbadec946b
Pedro Toribio <pedrom_t@hotmail.com>
2022-01-17 22:05:09 +0100

    [Btn styles] Adding the default configuration

--- src/App.js
+++ src/App.js
@@ -1,11 +1,18 @@
-
-import './App.css';
+import "./App.css";
+import Navbar from "./components/pages/Navbar";
+import {
+  BrowserRouter as Router,
+  Routes, Route
+} from "react-router-dom";
 
 function App() {
   return (
-    <div className="App">
-      <h1>Hello</h1>
-    </div>
+    <Router>
+      <Navbar />
+      <Routes>
+        <Route path="/" />
+      </Routes>
+    </Router>
   );
 }
 
