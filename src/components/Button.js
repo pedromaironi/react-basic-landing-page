0a4861b67e60b7d4ad592b65d28b0868b740d1e0
Pedro Toribio <pedrom_t@hotmail.com>
2022-01-18 18:34:35 +0100

    [Navbar] Adding the navbar

--- src/components/Button.js
+++ src/components/Button.js
@@ -1,11 +1,11 @@
 import React from "react";
-import './Button.css';
+import "./Button.css";
 
-const STYLES = ["btn--primary", "btn--outline"];
+const STYLES = ['btn--primary', 'btn--outline'];
 
-const SIZES = ["btn--medium", "btn--large", "btn--mobile", "btn--wide"];
+const SIZES = ['btn--medium', 'btn--large', 'btn--mobile', 'btn--wide'];
 
-const COLOR = ["primary", "blue", "red", "green"];
+const COLOR = ['primary', 'blue', 'red', 'green'];
 
 export const Button = ({
   children,
@@ -19,9 +19,9 @@ export const Button = ({
     ? buttonStyle
     : STYLES[0];
 
-  const checkButtonSize = STYLES.includes(buttonSize) ? buttonSize : SIZES[0];
+  const checkButtonSize = SIZES.includes(buttonSize) ? buttonSize : SIZES[0];
 
-  const checkButtonColor = COLOR.includes(buttonSize) ? buttonColor : COLOR[0];
+  const checkButtonColor = COLOR.includes(buttonColor) ? buttonColor : null;
 
   return (
     <button
